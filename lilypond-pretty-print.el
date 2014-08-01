b94760dd905397e7b2f7c78032d5dc49c5cdf1b5
knupfer <fknupfer@gmail.com>
2014-08-01 16:28:18 +0200

    minor mode for highlighting beats

--- lilypond-pretty-print.el
+++ lilypond-pretty-print.el
@@ -38,7 +38,73 @@
         (when time-passed
           (while (> time-passed (- (current-column) indentation-column))
             (insert " "))))))
+  ;; make-string LENGTH INIT
   (forward-line))
+
+(defun lilypond-beat-remove ()
+  (dolist (ov (lilypond-beat--active-overlays))
+    (delete-overlay ov)))
+
+(defun lilypond-beat--active-overlays ()
+  (delq nil (mapcar
+             (lambda (ov)
+               (and (eq (overlay-get ov 'category)
+                        'lily-pretty)
+                    ov))
+             (overlays-in (point-min) (point-max)))))
+
+(defun lilypond-beat-show ()
+  (unless (active-minibuffer-window)
+    (save-excursion
+      (goto-char (window-start))
+      (while (re-search-forward "|" (window-end) t)
+        (save-excursion
+          (let ((line (line-number-at-pos))
+                (local-count 0)
+                (increment 4))
+            (back-to-indentation)
+            (while (and (eq line (line-number-at-pos))
+                        (< (+ increment (point)) (window-end)))
+              (cond ((= (mod local-count 4) 0)
+                     (lilypond--make-overlay (point) "|"))
+                    ((= (mod local-count 4) 2)
+                     (lilypond--make-overlay (point) "+"))
+                    ((= (mod local-count 4) 1)
+                     (lilypond--make-overlay (point) "."))
+                    ((= (mod local-count 4) 3)
+                     (lilypond--make-overlay (point) ".")))
+              (setq local-count (+ 1 local-count))
+              (forward-char increment))))))))
+
+(defun lilypond--make-overlay (line symbol)
+  "draw line at (line, col)"
+  (let ((original-pos (point)) ov prop)
+    (save-excursion
+      (goto-char line)
+      (when (looking-at " ")
+        (setq prop 'display
+              ov (make-overlay (point) (+ 1 (point))))
+        (when ov
+          (overlay-put
+           ov 'category 'lily-pretty)
+          (overlay-put
+           ov prop
+           (propertize symbol 'face 'warning)))))))
+
+(define-minor-mode lilypond-pretty-beat-mode
+  :init-value nil
+  :lighter ".+.|.+."
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "M-q")
+              'lilypond-pretty-print)
+            map)
+  :global nil
+  (if lilypond-pretty-beat-mode
+      (progn
+        (add-hook 'pre-command-hook 'lilypond-beat-remove nil t)
+        (add-hook 'post-command-hook 'lilypond-beat-show nil t))
+    (remove-hook 'pre-command-hook 'lilypond-beat-remove t)
+    (remove-hook 'post-command-hook 'lilypond-beat-show t)))
 
 (provide 'lilypond-pretty-print)
 
